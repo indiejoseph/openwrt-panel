ec49b3a8087195762dc4869eb8f6b5187fc36bb6
indiejoseph <indiejoseph@gmail.com>
2013-08-12 23:28:40 +0800

    Updated app.lua

--- app.lua
+++ app.lua
@@ -12,6 +12,7 @@ require "log".setlevel('ALL', 'HTTP')
 --require "strict"
 
 local sched = require "sched"
+local json = require "lib/dkjson"
 local img_path, script_path, style_path = 'dist/images', 'dist/scripts', 'dist/styles'
 
 require "tasks/selector".init({service='luasocket'})
@@ -28,6 +29,29 @@ else
 	http_server.serve_static_content_from_ram('/images/', img_path)
 	http_server.serve_static_content_from_ram('/scripts/', script_path)
 	http_server.serve_static_content_from_ram('/styles/', style_path)
+end
+
+function print_r(arr, indentLevel)
+	local str = ""
+	local indentStr = "#"
+
+	if(indentLevel == nil) then
+		print(print_r(arr, 0))
+		return
+	end
+
+	for i = 0, indentLevel do
+		indentStr = indentStr.."\t"
+	end
+
+	for index,value in pairs(arr) do
+		if type(value) == "table" then
+			str = str..indentStr..index..": \n"..print_r(value, (indentLevel + 1))
+		else 
+			str = str..indentStr..index..": "..value.."\n"
+		end
+	end
+	return str
 end
 
 sched.sigrun({emitter='*', events={sched.EVENT_DIE}}, print)
@@ -64,19 +88,74 @@ end)
 
 --/api/cards GET
 http_server.set_request_handler('GET', '/api/cards', function(path, http_params, http_header)
-	env = assert (luasql.sqlite3())
-	conn = assert (env:connect("cards.sqlite"))
+	local cards = {}
+	local env = assert (luasql.sqlite3())
+	local conn = assert (env:connect("db/openwrt.db"))
 
-	return 200
+	local cur = assert(conn:execute("SELECT * FROM cards"))
+	local row = cur:fetch({})
+
+	while row do
+		card = { ["id"] = row[1], ["name"] = row[2] }
+		table.insert(cards, card)
+		row = cur:fetch(row)
+	end
+
+	content = json.encode(cards)
+
+	cur:close()
+	conn:close()
+	env:close()
+
+	return 200, {['Content-Type']='application/json', ['Content-Length']=#content}, content
 end)
 
 --/api/cards POST
 http_server.set_request_handler('POST', '/api/cards', function(path, http_params, http_header)
-	env = assert (luasql.sqlite3())
-	conn = assert (env:connect("cards.sqlite"))
+
+	local env = assert (luasql.sqlite3())
+	local conn = assert (env:connect("db/openwrt.db"))
+
+	conn:close()
+	env:close()
 	
 	return 200
 end)
+
+--/api/cards PUT
+http_server.set_request_handler('PUT', '/api/cards', function(path, http_params, http_header)
+
+	local env = assert (luasql.sqlite3())
+	local conn = assert (env:connect("db/openwrt.db"))
+
+	conn:close()
+	env:close()
+	
+	return 200
+end)
+
+--/api/cards DELETE
+http_server.set_request_handler('DELETE', '/api/cards', function(path, http_params, http_header)
+
+	local env = assert (luasql.sqlite3())
+	local conn = assert (env:connect("db/openwrt.db"))
+
+	conn:close()
+	env:close()
+	
+	return 200
+end)
+
+-- Initialize database
+env = assert (luasql.sqlite3())
+conn = assert (env:connect("db/openwrt.db"))
+assert(conn:execute("PRAGMA foreign_keys = ON"))
+assert(conn:execute("CREATE TABLE IF NOT EXISTS cards( id INTEGER UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(32) NOT NULL )"))
+assert(conn:execute("CREATE TABLE IF NOT EXISTS inputs(	id INTEGER UNIQUE NOT NULL PRIMARY KEY,	name VARCHAR(32) NOT NULL, type VARCHAR(12) NOT NULL, value BLOB NOT NULL, card_id INTEGER NOT NULL, FOREIGN KEY(card_id) REFERENCES cards(id) )"))
+-- assert(conn:execute("INSERT INTO cards VALUES (NULL, 'Demo 1')"))
+-- assert(conn:execute("INSERT INTO cards VALUES (NULL, 'Demo 2')"))
+conn:close()
+env:close()
 
 local conf = {
 	ip='0.0.0.0', 
